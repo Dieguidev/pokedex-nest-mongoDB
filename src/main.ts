3c8d80e6b83fd36e0bfb9f3798bd2dad2354f563
Dieguidev <diegogaraycullas@gmail.com>
2023-10-06 21:36:09 -0500

    get post patch terminated

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //whitelist elimina toda la data extra que no debe existir
+      whitelist: true,
+      // forbidNonWhitelisted manda un error si se envian datos extra
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
